17b1ce01d50f62ace4b4d6fc88e8cb7fa211667a
alien <kabdualieva2002@gmail.com>
2022-08-16 19:19:11 +0600

    Use breakpoints to make it responsive

--- script.js
+++ script.js
@@ -1,13 +1,18 @@
 const burger = document.querySelector(".header_burger")
 const head_menu = document.querySelector(".header_menu")
+const lock_body = document.querySelector("body")
 
 
 burger.addEventListener("click", () => {
     burger.classList.toggle("active");
     head_menu.classList.toggle("active");
+    lock_body.classList.toggle('lock')
+
 })
 
 document.querySelectorAll(".nav-link").forEach(n => n.addEventListener("click", () =>{
     burger.classList.remove("active");
     head_menu.classList.remove("active");
+    lock_body.classList.remove('lock')
+
 }))
